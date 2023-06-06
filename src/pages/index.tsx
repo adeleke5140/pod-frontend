4447ac207eb9f7bf46ef561d704ef4d3eee27bfb
Kenny <adelekekehinde06@gmail.com>
2023-06-06 23:34:24 +0100

    feat: remove form and useLink Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Layout from "~/components/layouts";
+import { ArrowRight } from "react-feather";
 
 const Home: NextPage = () => {
   return (
@@ -11,26 +12,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="mx-auto my-0 mt-16 flex max-w-6xl flex-col gap-8 text-center">
+        <div className="mx-auto my-0 mt-12 flex max-w-6xl flex-col gap-8 text-center">
           <div>
-            <h1 className="text-center font-bespoke text-7xl font-bold capitalize sm:mt-36">
+            <h1 className="text-center font-bespoke text-5xl font-bold capitalize md:mt-36 md:text-9xl">
               Proof of <span className="text-blue-600">development</span>
             </h1>
-            <p className="mt-2 text-lg">
+            <p className="mt-8 text-lg sm:text-xl">
               Get rewarded for your contributions to the open source ecosystem.
             </p>
           </div>
-          <form
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-            className="flex justify-center"
-          >
-            <button className="flex cursor-pointer items-center justify-center gap-2 rounded bg-blue-600 px-5 py-3 text-xl font-semibold text-white transition-colors ease-out hover:bg-blue-700 disabled:opacity-50">
-              Connect Repo
-            </button>
-          </form>
+
+          <button className="group flex cursor-pointer items-center justify-center gap-2 self-center rounded-3xl bg-blue-600 px-5 py-3 text-xl font-semibold text-white transition-colors ease-out hover:bg-blue-700 disabled:opacity-50">
+            Let&apos;s go
+            <ArrowRight className="transition-transform group-hover:translate-x-1" />
+          </button>
         </div>
       </Layout>
     </>
