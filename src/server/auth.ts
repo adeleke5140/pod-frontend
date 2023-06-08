78ebf4ddfeed75f408b8ae00cc1416fa6c5eca0b
Kenny <adelekekehinde06@gmail.com>
2023-06-08 04:06:18 +0100

    chore: remove discord provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -44,10 +44,7 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    
     /**
      * ...add more providers here.
      *
