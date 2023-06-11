abe67a887f793002748cf381df09307d2c4e919c
Kenny <adelekekehinde06@gmail.com>
2023-06-11 21:28:07 +0100

    chore: cleanup _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
-import { type Session } from "next-auth";
-import { type AppType } from "next/app";
+import type { AppProps } from "next/app";
 import "~/styles/globals.css";
 import { bespokeStencil, supremeFont } from "~/fonts/setup";
 import { ThemeProvider } from "degen";
 import "degen/styles";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider>
       <div className={`${bespokeStencil.variable} ${supremeFont.variable}`}>
