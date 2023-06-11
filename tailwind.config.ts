d23f0c81cc7d61f6442cbf077b4db47cf705531e
Kenny <adelekekehinde06@gmail.com>
2023-06-11 23:03:47 +0100

    feat: extend radix colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,18 @@
 import { type Config } from "tailwindcss";
+import { blackA, violet, mauve } from "@radix-ui/colors";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        bespoke: ['var(--font-bespoke)'],
-        supreme: ['var(--font-supreme)'],
+      fontFamily: {
+        bespoke: ["var(--font-bespoke)"],
+        supreme: ["var(--font-supreme)"],
+      },
+      colors: {
+        ...blackA,
+        ...violet,
+        ...mauve
       }
     },
   },
