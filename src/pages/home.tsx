345c0fcad8de6663e91cec50cd4489d90b4d31c9
Kenny <adelekekehinde06@gmail.com>
2023-06-11 14:10:35 +0100

    feat: update homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,21 +2,35 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Layout from "~/components/layouts";
 import { ArrowRight, UserCheck } from "react-feather";
-import Link from "next/link";
-import { redirectURL } from '../constants'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { redirectURL } from "../constants";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useCode, useAuthActions, useRepos } from "~/lib/zustand/codeSlice";
 
 const Home: NextPage = () => {
-  const router = useRouter()
-  const [code, setCode] = useState('')
+  const router = useRouter();
+  const code = useCode();
+  const { setCode, fetchAccessToken, fetchRepos } = useAuthActions();
+  const repos = useRepos();
 
+  console.log(repos);
   useEffect(() => {
-    const code = router.query.code
-    if(code){
-      setCode(code)
+    const code = router.query.code;
+    if (code && !Array.isArray(code)) {
+      setCode(code);
+      console.log(code);
     }
-  },[router])
+    void (async () => {
+      code && (await fetchAccessToken());
+      code && (await fetchRepos());
+    })();
+  }, [router, setCode, fetchAccessToken, fetchRepos]);
+
+  const clearCode = () => {
+    setCode("");
+    void router.replace("/home", undefined, { shallow: true });
+  };
+
   return (
     <>
       <Head>
@@ -25,23 +39,42 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="flex text-left flex-col gap-8 mt-16">
-          <h1 className="text-7xl font-bold font-bespoke">
-          {code ? "Welome to POD!" : "Authorize"}
+        <div className="mt-16 flex flex-col gap-8 text-left">
+          <h1 className="font-bespoke text-7xl font-bold">
+            {code ? "Welome to POD!" : "Authorize"}
           </h1>
-          <a
-            href={redirectURL}
-            className="group flex self-start cursor-pointer gap-2 rounded-3xl bg-blue-600 px-5 py-3 text-xl font-semibold text-white transition-colors ease-out hover:bg-blue-700 disabled:opacity-50"
-          >
-            {code ? 'Authorized' : 'Authorize with Github'}
-            {!code ? <ArrowRight className="transition-transform group-hover:translate-x-1" /> : <UserCheck/>}
-          </a>
-          code: {code}
+          <div className="flex gap-4">
+            {" "}
+            <a
+              href={redirectURL}
+              className={`${
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                code ? "pointer-events-none" : null
+              } group flex cursor-pointer gap-2 self-start rounded-3xl bg-blue-600 px-5 py-3 text-xl font-semibold text-white transition-colors ease-out hover:bg-blue-700 disabled:opacity-50`}
+            >
+              {code ? "Authorized" : "Authorize with Github"}
+              {!code ? (
+                <ArrowRight className="transition-transform group-hover:translate-x-1" />
+              ) : (
+                <UserCheck />
+              )}
+            </a>
+            {code && (
+              <button
+                onClick={clearCode}
+                className="group flex cursor-pointer gap-2 self-start rounded-3xl bg-red-500 px-5 py-3 text-xl font-semibold text-white transition-colors ease-out hover:bg-red-600 disabled:opacity-50"
+              >
+                Disconnect
+              </button>
+            )}
+          </div>
         </div>
         <div className="text-left">
           <h2 className="text-3xl font-semibold">Repositories</h2>
-          <p className="text-md">Select one that you would like to get contributions for and reward devs with PODs</p>
-
+          <p className="text-md">
+            Select one that you would like to get contributions for and reward
+            devs with PODs
+          </p>
         </div>
       </Layout>
     </>
