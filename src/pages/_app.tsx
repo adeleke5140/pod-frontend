4efdcefab6a1062a451f6475d5f351e477607633
Kenny <adelekekehinde06@gmail.com>
2023-06-11 23:04:20 +0100

    fix: remove degen imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import type { AppProps } from "next/app";
 import "~/styles/globals.css";
 import { bespokeStencil, supremeFont } from "~/fonts/setup";
-import { ThemeProvider } from "degen";
-import "degen/styles";
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
-      <div className={`${bespokeStencil.variable} ${supremeFont.variable}`}>
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
+    <div className={`${bespokeStencil.variable} ${supremeFont.variable}`}>
+      <Component {...pageProps} />
+    </div>
   );
 };
 
