7fb4c05a0533ec428054ed2dcc0f6c7373c9317c
Kenny <adelekekehinde06@gmail.com>
2023-06-11 14:10:01 +0100

    feat: add url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 
-const clientID = process.env.client_id
-const redirectURI = process.env.redirect_uri
+const clientID = process.env.client_id as string
+const redirectURI = process.env.redirect_uri as string
 export const redirectURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientID}&redirect_uri=${redirectURI}`
+export const githubCallbackURL = 'http://localhost:3058/user/v1-github-callback'
+export const githubRepoURL = 'http://localhost:3058/user/v1-get-public-repos-by-user'
