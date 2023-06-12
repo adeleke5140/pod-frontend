42153e278a20105dc5eca6e2353d9ea57e6ad00b
Kenny <adelekekehinde06@gmail.com>
2023-06-12 18:41:58 +0100

    feat:update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
+const clientID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+const redirectURI = process.env.NEXT_PUBLIC_REDIRECT_URI as string;
 
-const clientID = process.env.client_id as string
-const redirectURI = process.env.redirect_uri as string
-export const redirectURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientID}&redirect_uri=${redirectURI}`
-export const githubCallbackURL = 'http://localhost:3058/user/v1-github-callback'
-export const githubRepoURL = 'http://localhost:3058/user/v1-get-public-repos-by-user'
+export const redirectURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientID}&redirect_uri=${redirectURI}`;
+export const githubCallbackURL =
+  "http://localhost:3058/user/v1-github-callback";
+export const githubRepoURL =
+  "http://localhost:3058/user/v1-get-public-repos-by-user";
+export const web3StorageToken = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN;
