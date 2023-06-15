c421f0e6b716be563f141d9d4b3bd0835f58748b
Kenny <adelekekehinde06@gmail.com>
2023-06-15 22:11:16 +0100

    feat: add cards to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,9 +17,9 @@ const Index: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="mx-auto my-0 flex max-w-2xl flex-col gap-8 text-center">
+        <div className="mx-auto my-0 flex max-w-4xl flex-col gap-8 text-center">
           <div>
-            <h1 className="text-center font-bespoke text-5xl font-bold capitalize md:mt-[6%] md:text-8xl">
+            <h1 className="text-center font-bespoke text-5xl font-bold capitalize md:mt-[2%] md:text-8xl">
               Proof of <span className="text-blue-600">development</span>
             </h1>
             <p className="mt-8 text-lg sm:text-xl">
@@ -36,6 +36,29 @@ const Index: NextPage = () => {
             Get Started
             <ArrowRight className="transition-transform group-hover:translate-x-1" />
           </button>
+
+          <div className="pb-8 mt-6 text-left">
+            <div className="flex flex-col md:flex-row gap-6 md:gap-[2%]">
+              <section className="cursor:default select-none md:basis-[32rem] p-4 border-2 border-gray-100 border-blue-500 rounded-lg  hover:border-blue-500 transition-all">
+                <h2 className="mb-2 font-bespoke text-xl font-semibold">Create your <span className="text-blue-600">POD</span></h2>
+                <p>
+                  Create a <span className="text-blue-600">POD</span> for your open source project.
+                </p>
+              </section>
+              <section className="cursor:default select-none p-4 md:basis-[32rem] border-2 border-gray-100 border-blue-500 rounded-lg hover:border-blue-500 transition-colors">
+                <h2 className="mb-2 font-bespoke text-xl font-semibold">Get <span className="text-blue-600">Unique</span> Link</h2>
+                <p>
+                  Get a unique link for your <span className="text-blue-600">POD</span> and share it with your community/contributors.
+                </p>
+              </section>
+              <section className="cursor:default select-none p-4 md:basis-[32rem] border-2  border-gray-100 border-blue-500 rounded-lg hover:border-blue-500 transition-colors">
+                <h2 className="mb-2  font-bespoke text-xl font-semibold">Contributors Mint <span className="text-blue-600">POD</span></h2>
+                <p>
+                  We verify the contributions made and mint <span className="text-blue-600">POD</span> for the contributors.
+                </p>
+              </section>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
