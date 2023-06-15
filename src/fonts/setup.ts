862fad8c9f451398a4f523daffa93a89a26421b5
Kenny <adelekekehinde06@gmail.com>
2023-06-16 00:03:37 +0100

    style: add italic font

--- src/fonts/setup.ts
+++ src/fonts/setup.ts
@@ -1,19 +1,23 @@
 import localFont from "next/font/local";
 
 export const bespokeStencil = localFont({
-    src: [{
-        path:'./BespokeStencil/BespokeStencil-Variable.ttf',
-        style: 'normal',
-        weight: '400 500 600 700',
-    }],
-    variable: '--font-bespoke'
+  src: [{
+    path: './BespokeStencil/BespokeStencil-Variable.ttf',
+    style: 'normal',
+    weight: '400 500 600 700',
+  }, {
+    path: './BespokeStencil/BespokeStencil-VariableItalic.ttf',
+    style: 'italic',
+    weight: '400 500 600 700',
+  }],
+  variable: '--font-bespoke'
 })
 
 export const supremeFont = localFont({
-    src: [{
-        path:'./Supreme/Supreme-Variable.ttf',
-        style: 'normal',
-        weight: '400 500 600 700',
-    }],
-        variable: '--font-supreme'
+  src: [{
+    path: './Supreme/Supreme-Variable.ttf',
+    style: 'normal',
+    weight: '400 500 600 700',
+  }],
+  variable: '--font-supreme'
 })
