b88032fe345a14b8be4674b00cf2394028fcfa5c
Kenny <adelekekehinde06@gmail.com>
2023-06-15 22:10:57 +0100

    chore: remove unused imports

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+
 import { useMemo } from "react";
 import Layout from "~/components/layouts";
 import { useAccessToken, useCode } from "~/lib/zustand/codeSlice";
