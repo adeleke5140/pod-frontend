c49c142cd99a4c298a6fd4e0d26cfe3257b11db0
Kenny <adelekekehinde06@gmail.com>
2023-06-16 18:34:44 +0100

    feat: update mint page

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,17 +1,15 @@
-import { ConnectKitButton } from "connectkit"
+import { ConnectKitButton, useChains } from "connectkit"
 import { useEffect, useState } from "react"
 import Layout from "~/components/layouts"
-import { redirectURL } from "~/constants"
+import { redirectMintURL } from "~/constants"
 import { useRouter } from "next/router"
 import { ArrowRight } from "react-feather"
-import { useMintActions, useMintCode, useMintEligibility, useMintLoading, useMintTransactionHash } from "~/lib/zustand/mintSlice"
+import { useMintActions, useMintCode, useMintEligibility, useMintLoading, useMintTransactionHash, useProjectHash } from "~/lib/zustand/mintSlice"
 import * as Form from "@radix-ui/react-form";
 import { Spinner } from "~/components/spinner"
 import toast from "react-hot-toast"
 
-// create another store
-// it's gonna store the hash we get from the url
-// it's gonna store the code
+
 
 const MintPage = () => {
   const router = useRouter()
@@ -21,6 +19,11 @@ const MintPage = () => {
   const transactionHash = useMintTransactionHash()
   const { setProjectHash, setCode, setWalletAddress, checkMintEligibility } = useMintActions()
   const [mintState, setMintState] = useState('Check eligibility')
+  const chains = useChains()
+  const projectHash = useProjectHash()
+  console.log({
+    projectHash
+  })
 
   useEffect(() => {
     const pHash = router.query.pHash
@@ -44,24 +47,47 @@ const MintPage = () => {
     }
   }, [mintEligibility])
 
+  // async function resolveEnsName(data: string) {
+  //   const address = await viemClient.getEnsResolver(
+  //     { name: normalize(data) }
+  //   )
+  //   console.log(address)
+  //   return address
+  // }
+
+
   async function handleMint(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     const data = Object.fromEntries(
       new FormData(e.currentTarget)
-    ) as unknown as string
+    ) as unknown as { 'wallet-address': string }
 
-
-    setWalletAddress(data)
-    await checkMintEligibility()
+    console.log('started here')
+    console.log(data)
+    if (data['wallet-address'].includes('.eth')) {
+      toast.error('Ens name not supported yet')
+    } else {
+      setWalletAddress(data['wallet-address'])
+    }
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    // await checkMintEligibility()
   }
 
   return (
     <Layout>
-      <div className="flex flex-col gap-4 max-w-xl mx-auto my-0">
-        <h1 className="font-bespoke text-7xl font-bold">Mint your <span className="text-blue-500">POD</span></h1>
+      <div className="flex flex-col gap-4 max-w-4xl mx-auto my-0">
+        <div>
+          <h1 className="font-bespoke text-7xl font-bold">Mint <span className="text-blue-500">POD</span></h1>
+          <div className="text-left mt-2">
+            <span className="opacity-50 text-sm">Supported Network:</span>{' '}
+            {chains.map(chain => (
+              <code className="bg-gray-100 text-xs inset-2 rounded-lg px-2 py-2" key={chain.id}>{chain.name}</code>
+            ))}
+          </div>
+        </div>
         <div className="flex justify-center align-center">
-          {code ? <a
-            href={redirectURL}
+          {!code ? <a
+            href={redirectMintURL}
             className="text-md group flex cursor-pointer gap-2 self-start rounded-3xl bg-blue-600 px-4 py-2 font-semibold text-white transition-colors ease-out hover:bg-blue-700 disabled:opacity-50"
           >
             <span className="font-supreme">Connect Github</span>
@@ -75,8 +101,9 @@ const MintPage = () => {
         </div>
         <div className="mt-6">
           {code ?
-            <Form.Root className="flex flex-col gap-4" onSubmit={void handleMint}>
-              <Form.Field name="wallet-adress">
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            <Form.Root className="flex flex-col gap-4" onSubmit={handleMint}>
+              <Form.Field name="wallet-address">
                 <div className="flex items-baseline justify-between">
                   <Form.Label className="text-[15px] font-medium leading-[35px] font-supreme">Wallet Address</Form.Label>
                   <Form.Message className="text-[13px] text-red-500 opacity-[0.9]" match="valueMissing">
@@ -85,7 +112,7 @@ const MintPage = () => {
                 </div>
                 <Form.Control asChild>
                   <input
-                    className="selection:color-black box-border inline-flex h-[50px] w-full resize-none appearance-none items-center justify-center rounded-xl p-[10px] text-xl leading-none shadow-[0_0_0_1.2px] shadow-gray-400 outline-none selection:bg-blue-100  focus:ring-2 focus:ring-blue-500"
+                    className="selection:color-black pl-4 text-base font-supreme box-border inline-flex h-[50px] w-full resize-none appearance-none items-center justify-center rounded-xl p-[10px] text-xl leading-none shadow-[0_0_0_1.2px] shadow-gray-400 outline-none selection:bg-blue-100  focus:ring-2 focus:ring-blue-500"
                     type="text"
                     required
                   />
