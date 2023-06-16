20a5545c50f363d1f4440e2a26c693bfcb5749b0
Kenny <adelekekehinde06@gmail.com>
2023-06-16 13:50:54 +0100

    feat: update podSlice

--- src/lib/zustand/podSlice.ts
+++ src/lib/zustand/podSlice.ts
@@ -4,43 +4,56 @@ import { requestProjectApprovalURL } from "~/constants";
 import type { CIDString } from "web3.storage";
 import { useAuthStore } from "./codeSlice";
 
+//pHash - projectHash
+//tHash - transactionHash
+
 interface PodState {
+  loading: boolean;
   podCreationSuccess: boolean
   podCreationFailure: boolean
-  podCreationProjectHash: string
+  podCreationDetails: {
+    pHash: string;
+    tHash: string
+  }
   podDetails: {
-    userId: string;
     projectName: string;
     signature: unknown;
     nftUri: CIDString;
     minContributions: number;
   } | null;
   actions: {
+    startLoading: () => void;
+    stopLoading: () => void;
     setPodDetails: (podDetails: PodState["podDetails"]) => void;
     createPod: () => Promise<void>;
   };
 }
 
 interface PodCreationServerResponse {
+  success: boolean;
   data: {
-    success: boolean;
-    data: {
-      projectHash: string;
-      transactionHash: string;
-    }
-    message: string;
-  };
+    projectHash: string;
+    transactionHash: string;
+  }
+  message: string;
 }
 
 export const usePodSlice = create<PodState>()((set, get) => ({
-  podCreationSuccess: true,
+  loading: false,
+  podCreationSuccess: false,
   podCreationFailure: false,
-  podCreationProjectHash: "",
+  podCreationDetails: {
+    pHash: "",
+    tHash: ""
+  },
   podDetails: null,
   actions: {
     setPodDetails: (podDetails) => set({ podDetails }),
+    startLoading: () => set((state) => ({ ...state, loading: true })),
+    stopLoading: () => set((state) => ({ ...state, loading: false })),
     createPod: async () => {
       const podDetails = get().podDetails;
+      const stopLoading = get().actions.stopLoading;
       const { access_token } = useAuthStore.getState();
       if (!podDetails) {
         return;
@@ -59,18 +72,32 @@ export const usePodSlice = create<PodState>()((set, get) => ({
             },
           }
         );
-        if (data.data.success) {
+        if (data.success) {
+          stopLoading()
           set((state) => ({ ...state, podCreationSuccess: true }))
-          set((state) => ({ ...state, podCreationProjectHash: data.data.data.projectHash }))
+          set((state) => ({
+            ...state, podCreationDetails: {
+              ...state.podCreationDetails,
+              pHash: data.data.projectHash,
+            }
+          }))
+          set((state) => ({
+            ...state, podCreationDetails: {
+              ...state.podCreationDetails,
+              tHash: data.data.transactionHash,
+            }
+          }))
           console.log("pod created🎉");
           console.log(data);
         } else {
+          stopLoading()
           set((state) => ({ ...state, podCreationFailure: true }))
           console.log("pod creation failed👎");
           console.log(data);
           throw new Error("pod creation failed");
         }
       } catch (err) {
+        stopLoading()
         console.error("Cannot create POD", err);
       }
     },
@@ -80,3 +107,5 @@ export const usePodSlice = create<PodState>()((set, get) => ({
 export const usePodActions = () => usePodSlice((state) => state.actions);
 export const usePodCreationSuccess = () => usePodSlice((state) => state.podCreationSuccess);
 export const usePodCreationFailure = () => usePodSlice((state) => state.podCreationFailure);
+export const usePodCreationDetails = () => usePodSlice((state) => state.podCreationDetails);
+export const useIsCreatingPod = () => usePodSlice((state) => state.loading);
