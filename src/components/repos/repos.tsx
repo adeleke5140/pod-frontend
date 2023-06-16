95b21a0b00205e9d0443c079fc693980916eae3a
Kenny <adelekekehinde06@gmail.com>
2023-06-16 23:23:51 +0100

    chore: move file to respective folder

--- src/components/repos/repos.tsx
+++ src/components/repos/repos.tsx
@@ -1,23 +1,8 @@
 import { useRepos, useLoading } from "~/lib/zustand/codeSlice";
 import Link from "next/link";
-import { Spinner } from "../spinner";
 import { useRouter } from "next/router";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
+import { SkeletonCard } from "../skeleton/skeletonCard";
 
-function SkeletonCard() {
-  return (
-    <>
-      {Array(6).fill().map((_, i) => (
-        <div key={i} className="mb-4 flex h-auto flex-col gap-3 rounded-lg border-2 border-gray-200 p-6 font-supreme shadow-transparent transition-colors hover:border-blue-600 hover:bg-slate-50">
-          <h3 className="text-2xl font-semibold"><Skeleton /></h3>
-          <span><Skeleton /></span>
-        </div>
-      ))
-      }
-    </>
-  )
-}
 const Repos = () => {
   const repos = useRepos();
   const loading = useLoading();
---
+++ src/components/skeleton/skeletonCard.tsx
@@ -0,0 +1,18 @@
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
+
+function SkeletonCard() {
+  return (
+    <>
+      {Array(6).fill().map((_, i) => (
+        <div key={i} className="mb-4 flex h-auto flex-col gap-3 rounded-lg border-2 border-gray-200 p-6 font-supreme shadow-transparent transition-colors hover:border-blue-600 hover:bg-slate-50">
+          <h3 className="text-2xl font-semibold"><Skeleton /></h3>
+          <span><Skeleton /></span>
+        </div>
+      ))
+      }
+    </>
+  )
+}
+
+export { SkeletonCard }
