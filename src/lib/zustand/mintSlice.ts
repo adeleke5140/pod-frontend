6a6c968803db5cdf93a87d510395aa87ba2f458c
Kenny <adelekekehinde06@gmail.com>
2023-06-16 18:25:48 +0100

    feat: persist in localStorage

--- src/lib/zustand/mintSlice.ts
+++ src/lib/zustand/mintSlice.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { checkMintEligibilityURL } from "~/constants";
 import axios from 'axios'
+import * as middleware from "zustand/middleware";
 
 interface MintState {
   loading: boolean;
@@ -29,50 +30,58 @@ interface MintEligibilityServerResponse {
   }
 }
 
-export const useMintSlice = create<MintState>((set, get) => ({
-  loading: false,
-  pHash: "",
-  code: "a",
-  walletAddress: "",
-  mintEligibility: false,
-  transactionHash: 'a',
-  actions: {
-    startLoading: () => set((state) => ({ ...state, loading: true })),
-    stopLoading: () => set((state) => ({ ...state, loading: false })),
-    setProjectHash: (pHash) => set((state) => ({ ...state, pHash })),
-    setCode: (code) => set((state) => ({ ...state, code })),
-    setWalletAddress: (walletAddress) => set((state) => ({ ...state, walletAddress })),
-    checkMintEligibility: async () => {
-      const startLoading = get().actions.startLoading;
-      const stopLoading = get().actions.stopLoading;
-      startLoading()
-      const { pHash, code, walletAddress } = get();
-      try {
-        const { data } = await axios.post<MintEligibilityServerResponse>(checkMintEligibilityURL, {
-          projectHash: pHash,
-          code,
-          account: walletAddress
-        })
-        if (data.data.success) {
-          stopLoading()
-          set((state) => ({ ...state, mintEligibility: data.data.data.isAllowedToMint }))
-          set((state) => ({ ...state, transactionHash: data.data.data.transactionHash }))
-          console.log('Allowed to mint', "✅")
-        } else {
-          stopLoading()
-          console.log('Not allowed to mint', "❌")
+export const useMintSlice = create<MintState>()(
+  middleware.persist(
+    (set, get) => ({
+      loading: false,
+      pHash: "",
+      code: "",
+      walletAddress: "",
+      mintEligibility: false,
+      transactionHash: "",
+      actions: {
+        startLoading: () => set((state) => ({ ...state, loading: true })),
+        stopLoading: () => set((state) => ({ ...state, loading: false })),
+        setProjectHash: (pHash) => set((state) => ({ ...state, pHash })),
+        setCode: (code) => set((state) => ({ ...state, code })),
+        setWalletAddress: (walletAddress) => set((state) => ({ ...state, walletAddress })),
+        checkMintEligibility: async () => {
+          const startLoading = get().actions.startLoading;
+          const stopLoading = get().actions.stopLoading;
+          startLoading()
+          const { pHash, code, walletAddress } = get();
+          try {
+            const { data } = await axios.post<MintEligibilityServerResponse>(checkMintEligibilityURL, {
+              projectHash: pHash,
+              code,
+              account: walletAddress
+            })
+            if (data.data.success) {
+              stopLoading()
+              set((state) => ({ ...state, mintEligibility: data.data.data.isAllowedToMint }))
+              set((state) => ({ ...state, transactionHash: data.data.data.transactionHash }))
+              set((state) => ({ ...state, pHash: "" }))
+              console.log('Allowed to mint', "✅")
+            } else {
+              stopLoading()
+              console.log('Not allowed to mint', "❌")
+            }
+          } catch (err) {
+            stopLoading()
+            console.error('Could not fetch Mint eligibility', err)
+          }
         }
-      } catch (err) {
-        stopLoading()
-
-        console.error('Could not fetch Mint eligibility', err)
       }
-    }
-  }
-}))
+    }),
+    {
+      name: "mint-storage",
+      storage: middleware.createJSONStorage(() => localStorage),
+      partialize: (state) => ({ pHash: state.pHash })
+    }))
 
 export const useMintLoading = () => useMintSlice((state) => state.loading)
 export const useMintEligibility = () => useMintSlice((state) => state.mintEligibility)
 export const useMintActions = () => useMintSlice((state) => state.actions)
 export const useMintCode = () => useMintSlice((state) => state.code)
 export const useMintTransactionHash = () => useMintSlice((state) => state.transactionHash)
+export const useProjectHash = () => useMintSlice((state) => state.pHash)
