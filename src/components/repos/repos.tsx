6e072959fc7878dd24336f3b5a2fa38d49380dd7
Kenny <adelekekehinde06@gmail.com>
2023-06-17 02:06:43 +0100

    feat: add filter component and verified logo

--- src/components/repos/repos.tsx
+++ src/components/repos/repos.tsx
@@ -2,22 +2,31 @@ import { useRepos, useLoading } from "~/lib/zustand/codeSlice";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { SkeletonCard } from "../skeleton/skeletonCard";
+import { useState } from "react";
 
 const Repos = () => {
+  const [repoDisplay, setRepoDisplay] = useState<'all' | 'pod'>('all')
   const repos = useRepos();
   const loading = useLoading();
   const router = useRouter();
+
+  const repoWithPod = repos.filter(repo => repo.isAllowedMint)
+
   return (
     <>
       <div className="text-left">
-        <h2 className="text-3xl font-semibold">Repositories</h2>
-        <p className="mb-8 text-lg">
+        <h2 className="text-2xl font-semibold">Repositories</h2>
+        <p className="mb-8 text-base">
           Select a repositories to associate with your POD.
         </p>
+        <ul className="flex gap-4 mb-4">
+          <button onClick={() => setRepoDisplay('all')} className={`cursor-pointer bg-blue-50 hover:bg-blue-100 transition-all rounded-lg px-2 py-1 opacity-60 font-semibold ${repoDisplay === 'all' ? "text-blue-600  bg-blue-100 opacity-100" : ''}`}>All repos</button>
+          <button onClick={() => setRepoDisplay('pod')} className={`cursor-pointer bg-blue-50 hover:bg-blue-100 transition-all rounded-lg px-2 py-1 opacity-60 font-semibold ${repoDisplay === 'pod' ? "text-blue-600  bg-blue-100 opacity-100" : ''}`}>POD repos</button>
+        </ul>
         <div className="grid gap-5 md:grid-cols-2">
           {!repos.length && !loading && <p>No repos yet...</p>}
           {loading && <SkeletonCard />}
-          {repos.map((repo) => (
+          {repoDisplay === 'all' ? repos.map((repo) => (
             <Link
               href={{
                 pathname: `/repos/${repo.name}`,
@@ -26,16 +35,54 @@ const Repos = () => {
               passHref={true}
               shallow={true}
               key={repo.name}
-              className="mb-4 flex h-auto flex-col gap-3 rounded-lg border-2 border-gray-200 p-6 font-supreme shadow-transparent transition-colors hover:border-blue-600 hover:bg-slate-50"
+              className={`${repo.isAllowedMint ? 'pointer-events-none' : ''} relative mb-4 flex h-auto flex-col gap-3 rounded-lg border-2 border-gray-200 p-6 font-supreme shadow-transparent transition-colors hover:border-blue-600 hover:bg-slate-50`}
             >
-              <h3 className="text-2xl font-semibold">{repo.name || <Skeleton />}</h3>
+              <h3 className="text-xl font-semibold">{repo.name}</h3>
               {repo.description ? (
-                <span>Details: {repo.description || <SkeletonCard />}</span>
+                <span>Details: {repo.description}</span>
               ) : (
                 <span>No description</span>
               )}
+              <div className="absolute right-4 top-4">
+                {repo.isAllowedMint ?
+                  <div className="flex flex-row-reverse items-center justify-center gap-2 bg-blue-100 rounded-lg text-blue-600 font-medium text-sm px-2 py-1">
+                    <span>Pod assigned</span>
+                    <svg viewBox="0 0 22 22"
+                      aria-label="Pod created"
+                      role="img"
+                      className="text-blue-500 h-4 w-4 fill-blue-500"
+                      data-testid="icon-verified">
+                      <g>
+                        <path d="M20.396 11c-.018-.646-.215-1.275-.57-1.816-.354-.54-.852-.972-1.438-1.246.223-.607.27-1.264.14-1.897-.131-.634-.437-1.218-.882-1.687-.47-.445-1.053-.75-1.687-.882-.633-.13-1.29-.083-1.897.14-.273-.587-.704-1.086-1.245-1.44S11.647 1.62 11 1.604c-.646.017-1.273.213-1.813.568s-.969.854-1.24 1.44c-.608-.223-1.267-.272-1.902-.14-.635.13-1.22.436-1.69.882-.445.47-.749 1.055-.878 1.688-.13.633-.08 1.29.144 1.896-.587.274-1.087.705-1.443 1.245-.356.54-.555 1.17-.574 1.817.02.647.218 1.276.574 1.817.356.54.856.972 1.443 1.245-.224.606-.274 1.263-.144 1.896.13.634.433 1.218.877 1.688.47.443 1.054.747 1.687.878.633.132 1.29.084 1.897-.136.274.586.705 1.084 1.246 1.439.54.354 1.17.551 1.816.569.647-.016 1.276-.213 1.817-.567s.972-.854 1.245-1.44c.604.239 1.266.296 1.903.164.636-.132 1.22-.447 1.68-.907.46-.46.776-1.044.908-1.681s.075-1.299-.165-1.903c.586-.274 1.084-.705 1.439-1.246.354-.54.551-1.17.569-1.816zM9.662 14.85l-3.429-3.428 1.293-1.302 2.072 2.072 4.4-4.794 1.347 1.246z">
+                        </path>
+                      </g>
+                    </svg></div> : null}
+              </div>
             </Link>
-          ))}
+          )) : repoWithPod.map(repo => (<div
+            key={repo.name}
+            className="relative mb-4 flex h-auto flex-col gap-3 rounded-lg border-2 border-gray-200 p-6 font-supreme shadow-transparent transition-colors hover:border-blue-600 hover:bg-slate-50"
+          >
+            <h3 className="text-xl font-semibold">{repo.name}</h3>
+            {repo.description ? (
+              <span>Details: {repo.description}</span>
+            ) : (
+              <span>No description</span>
+            )}
+            <div>
+              {repo.isAllowedMint ?
+                <svg viewBox="0 0 22 22"
+                  aria-label="Pod created"
+                  role="img"
+                  className="absolute right-4 top-4 text-blue-500 h-4 w-4 fill-blue-500"
+                  data-testid="icon-verified">
+                  <g>
+                    <path d="M20.396 11c-.018-.646-.215-1.275-.57-1.816-.354-.54-.852-.972-1.438-1.246.223-.607.27-1.264.14-1.897-.131-.634-.437-1.218-.882-1.687-.47-.445-1.053-.75-1.687-.882-.633-.13-1.29-.083-1.897.14-.273-.587-.704-1.086-1.245-1.44S11.647 1.62 11 1.604c-.646.017-1.273.213-1.813.568s-.969.854-1.24 1.44c-.608-.223-1.267-.272-1.902-.14-.635.13-1.22.436-1.69.882-.445.47-.749 1.055-.878 1.688-.13.633-.08 1.29.144 1.896-.587.274-1.087.705-1.443 1.245-.356.54-.555 1.17-.574 1.817.02.647.218 1.276.574 1.817.356.54.856.972 1.443 1.245-.224.606-.274 1.263-.144 1.896.13.634.433 1.218.877 1.688.47.443 1.054.747 1.687.878.633.132 1.29.084 1.897-.136.274.586.705 1.084 1.246 1.439.54.354 1.17.551 1.816.569.647-.016 1.276-.213 1.817-.567s.972-.854 1.245-1.44c.604.239 1.266.296 1.903.164.636-.132 1.22-.447 1.68-.907.46-.46.776-1.044.908-1.681s.075-1.299-.165-1.903c.586-.274 1.084-.705 1.439-1.246.354-.54.551-1.17.569-1.816zM9.662 14.85l-3.429-3.428 1.293-1.302 2.072 2.072 4.4-4.794 1.347 1.246z">
+                    </path>
+                  </g>
+                </svg> : null}
+            </div>
+          </div>))}
         </div>
       </div>
     </>
