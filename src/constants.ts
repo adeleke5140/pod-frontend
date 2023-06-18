35bf69e6d34dc06d5a9dd3726d59365e3783a8ea
heypran <pranjal.baweja@gmail.com>
2023-06-18 16:02:01 +0200

    feat: domain

--- src/constants.ts
+++ src/constants.ts
@@ -3,17 +3,14 @@ const clientIDMint = process.env.NEXT_PUBLIC_CLIENT_ID_MINT as string;
 const redirectURI = process.env.NEXT_PUBLIC_REDIRECT_URI as string;
 const redirectMintURI = process.env.NEXT_PUBLIC_REDIRECT_MINT_URI as string;
 
+export const domain = process.env.NEXT_PUBLIC_DOMAIN as string;
 export const redirectURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientID}&redirect_uri=${redirectURI}`;
 export const redirectMintURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientIDMint}&redirect_uri=${redirectMintURI}`;
-export const githubCallbackURL =
-  "http://localhost:3058/user/v1-github-callback";
-export const githubRepoURL =
-  "http://localhost:3058/user/v1-get-public-repos-by-user";
-export const requestProjectApprovalURL =
-  "http://localhost:3058/user/v1-request-project-approval";
-export const checkMintEligibilityURL =
-  "http://localhost:3058/user/v1-is-user-allowed-to-mint";
-export const domain = process.env.NEXT_PUBLIC_DOMAIN as string;
+export const githubCallbackURL = `http://${domain}:3058/user/v1-github-callback`;
+export const githubRepoURL = `http://${domain}:3058/user/v1-get-public-repos-by-user`;
+export const requestProjectApprovalURL = `http://${domain}:3058/user/v1-request-project-approval`;
+export const checkMintEligibilityURL = `http://${domain}:3058/user/v1-is-user-allowed-to-mint`;
+
 export const callibrationRpc = `https://filecoin-calibration.chainup.net/rpc/v1`;
 export const callibrationChainId = 12;
 export const podContract = "0x7748054C55C0b38e5Bd33de97E00141D542a54a7";
