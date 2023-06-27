d67f0cb7213d11ed4b7ef3d59bca90b5a20ca926
Kenny <adelekekehinde06@gmail.com>
2023-06-28 00:12:21 +0100

    feat: use zod to validate env

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,20 @@
-const clientID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
-const clientIDMint = process.env.NEXT_PUBLIC_CLIENT_ID_MINT as string;
-const redirectURI = process.env.NEXT_PUBLIC_REDIRECT_URI as string;
-const redirectMintURI = process.env.NEXT_PUBLIC_REDIRECT_MINT_URI as string;
+import { env } from "./env.mjs";
+const clientID = env.NEXT_PUBLIC_CLIENT_ID;
+const clientIDMint = env.NEXT_PUBLIC_CLIENT_ID_MINT;
+const redirectURI = env.NEXT_PUBLIC_CLIENT_REDIRECT_URI;
+const redirectMintURI = env.NEXT_PUBLIC_REDIRECT_MINT_URI;
+export const web3StorageToken = env.NEXT_PUBLIC_WEB3STORAGE_TOKEN;
+export const walletConnectId = env.NEXT_PUBLIC_WALLET_CONNECT_ID;
 
-export const mintDomain = process.env.NEXT_PUBLIC_DOMAIN as string;
-export const domain = process.env.NEXT_PUBLIC_API_URL as string;
+export const mintDomain = env.NEXT_PUBLIC_DOMAIN;
+export const apiUrl = env.NEXT_PUBLIC_API_URL;
 export const redirectURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientID}&redirect_uri=${redirectURI}`;
 export const redirectMintURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientIDMint}&redirect_uri=${redirectMintURI}`;
-export const githubCallbackURL = `${domain}/user/v1-github-callback`;
-export const githubRepoURL = `${domain}/user/v1-get-public-repos-by-user`;
-export const requestProjectApprovalURL = `${domain}/user/v1-request-project-approval`;
-export const checkMintEligibilityURL = `${domain}/user/v1-is-user-allowed-to-mint`;
+export const githubCallbackURL = `${apiUrl}/user/v1-github-callback`;
+export const githubRepoURL = `${apiUrl}/user/v1-get-public-repos-by-user`;
+export const requestProjectApprovalURL = `${apiUrl}/user/v1-request-project-approval`;
+export const checkMintEligibilityURL = `${apiUrl}/user/v1-is-user-allowed-to-mint`;
 
 export const callibrationRpc = `https://filecoin-calibration.chainup.net/rpc/v1`;
 export const callibrationChainId = 12;
 export const podContract = "0x7748054C55C0b38e5Bd33de97E00141D542a54a7";
-export const web3StorageToken = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN;
-export const walletConnectId = process.env
-  .NEXT_PUBLIC_WALLET_CONNECT_ID as string;
--- src/env.mjs
+++ src/env.mjs
@@ -2,40 +2,25 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
-  server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-    client_id: z.string(),
-    redirect_uri: z.string(),
-    web3StorageToken: z.string(),
-  },
-
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
+  server: {},
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_CLIENT_ID: z.string(),
+    NEXT_PUBLIC_CLIENT_ID_MINT: z.string(),
+    NEXT_PUBLIC_CLIENT_REDIRECT_URI: z.string().url(),
+    NEXT_PUBLIC_REDIRECT_MINT_URI: z.string().url(),
+    NEXT_PUBLIC_WEB3STORAGE_TOKEN: z.string(),
+    NEXT_PUBLIC_WALLET_CONNECT_ID: z.string(),
+    NEXT_PUBLIC_DOMAIN: z.string().url(),
+    NEXT_PUBLIC_API_URL: z.string().url(),
   },
-
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    client_id: process.env.client_id,
-    redirect_uri: process.env.redirect_uri,
-    web3StorageToken: process.env.web3StorageToken,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
+    NEXT_PUBLIC_CLIENT_ID_MINT: process.env.NEXT_CLIENT_ID_MINT,
+    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+    NEXT_PUBLIC_REDIRECT_MINT_URI: process.env.NEXT_PUBLIC_REDIRECT_MINT_URI,
+    NEXT_PUBLIC_CLIENT_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
+    NEXT_PUBLIC_WALLET_CONNECT_ID: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+    NEXT_PUBLIC_WEB3STORAGE_TOKEN: process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
-   * This is especially useful for Docker builds.
-   */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
